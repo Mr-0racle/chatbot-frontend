a8d2e9d14f915987475164b87318a395b35ef54d
Darren Pereira <darrenpereira9@gmail.com>
2023-12-22 17:41:11 +0530

    upgrade

--- app.js
+++ app.js
@@ -1,91 +1,162 @@
-class Chatbox {
+/* class Chatbox {
     constructor() {
         this.args = {
-            openButton: document.querySelector('.chatbox__button'),
+            openButton: document.querySelector('.chatbox__button button'),
             chatBox: document.querySelector('.chatbox__support'),
-            sendButton: document.querySelector('.send__button')
+            sendButton: document.querySelector('.send__button'),
+            typingIndicator: document.querySelector('.typing-indicator'),
+            inputField: document.querySelector('.chatbox__input--footer')
         }
 
         this.state = false;
         this.messages = [];
+
+        this.display();
     }
 
     display() {
-        const {openButton, chatBox, sendButton} = this.args;
+        const { openButton, chatBox, sendButton, inputField } = this.args;
 
-        openButton.addEventListener('click', () => this.toggleState(chatBox))
+        openButton.addEventListener('click', () => this.toggleState(chatBox));
 
-        sendButton.addEventListener('click', () => this.onSendButton(chatBox))
+        sendButton.addEventListener('click', () => {
+            this.sendMessage();
+        });
 
-        const node = chatBox.querySelector('input');
-        node.addEventListener("keyup", ({key}) => {
+        inputField.addEventListener("keyup", ({ key }) => {
             if (key === "Enter") {
-                this.onSendButton(chatBox)
+                this.sendMessage();
             }
-        })
+        });
     }
 
     toggleState(chatbox) {
         this.state = !this.state;
 
-        // show or hides the box
-        if(this.state) {
-            chatbox.classList.add('chatbox--active')
+        if (this.state) {
+            chatbox.classList.add('chatbox--active');
         } else {
-            chatbox.classList.remove('chatbox--active')
+            chatbox.classList.remove('chatbox--active');
         }
     }
 
-    onSendButton(chatbox) {
-        var textField = chatbox.querySelector('input');
-        let text1 = textField.value
-        if (text1 === "") {
+    sendMessage() {
+        const textField = this.args.inputField;
+        const message = textField.value.trim();
+
+        if (message === "") {
             return;
         }
 
-        let msg1 = { name: "User", message: text1 }
-        this.messages.push(msg1);
+        const userMessage = { name: "User", message };
+        this.messages.push(userMessage);
 
-        fetch('http://127.0.0.1:5000/predict', {
-            method: 'POST',
-            body: JSON.stringify({ message: text1 }),
-            mode: 'cors',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-          })
-          .then(r => r.json())
-          .then(r => {
-            let msg2 = { name: "Sam", message: r.answer };
-            this.messages.push(msg2);
-            this.updateChatText(chatbox)
-            textField.value = ''
+        // Simulated response from server (replace this with actual fetch call)
+        const samResponse = { name: "Sam", message: "This is a response from Sam." };
+        this.messages.push(samResponse);
 
-        }).catch((error) => {
-            console.error('Error:', error);
-            this.updateChatText(chatbox)
-            textField.value = ''
-          });
+        this.updateChatText();
+        textField.value = '';
     }
 
-    updateChatText(chatbox) {
-        var html = '';
-        this.messages.slice().reverse().forEach(function(item, index) {
-            if (item.name === "Sam")
-            {
-                html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>'
+    updateChatText() {
+        let html = '';
+        this.messages.forEach(function(item) {
+            if (item.name === "Sam") {
+                html += `<div class="messages__item messages__item--visitor">${item.message}</div>`;
+            } else {
+                html += `<div class="messages__item messages__item--operator">${item.message}</div>`;
             }
-            else
-            {
-                html += '<div class="messages__item messages__item--operator">' + item.message + '</div>'
-            }
-          });
+        });
 
-        const chatmessage = chatbox.querySelector('.chatbox__messages');
-        chatmessage.innerHTML = html;
+        const chatMessage = this.args.chatBox.querySelector('.chatbox__messages');
+        chatMessage.innerHTML = html;
+
+        // Scroll to the bottom to show the most recent message
+        chatMessage.scrollTop = chatMessage.scrollHeight;
     }
 }
 
+const chatbox = new Chatbox();
+ */
+
+class Chatbox {
+    constructor() {
+        this.args = {
+            openButton: document.querySelector('.chatbox__button button'),
+            chatBox: document.querySelector('.chatbox__support'),
+            sendButton: document.querySelector('.send__button'),
+            typingIndicator: document.querySelector('.typing-indicator'),
+            inputField: document.querySelector('.chatbox__input--footer')
+        }
+
+        this.state = false;
+        this.messages = [];
+
+        this.display();
+    }
+
+    display() {
+        const { openButton, chatBox, sendButton, inputField } = this.args;
+
+        openButton.addEventListener('click', () => this.toggleState(chatBox));
+
+        sendButton.addEventListener('click', () => {
+            this.sendMessage();
+        });
+
+        inputField.addEventListener("keyup", ({ key }) => {
+            if (key === "Enter") {
+                this.sendMessage();
+            }
+        });
+    }
+
+    toggleState(chatbox) {
+        this.state = !this.state;
+
+        if (this.state) {
+            chatbox.classList.add('chatbox--active');
+        } else {
+            chatbox.classList.remove('chatbox--active');
+        }
+    }
+
+    sendMessage() {
+        const textField = this.args.inputField;
+        const message = textField.value.trim();
+
+        if (message === "") {
+            return;
+        }
+
+        // Create a new user message object
+        const userMessage = { name: "User", message };
+
+        // Push the user's message to the end of the messages array
+        this.messages.push(userMessage);
+
+        // Update the chat display
+        this.updateChatText();
+        textField.value = '';
+    }
+
+    updateChatText() {
+        let html = '';
+
+        for (const message of this.messages) {
+            if (message.name === "Sam") {
+                html += `<div class="messages__item messages__item--operator">${message.message}</div>`;
+            } else {
+                html = `<div class="messages__item messages__item--visitor">${message.message}</div>` + html;
+            }
+        }
+
+        const chatMessage = this.args.chatBox.querySelector('.chatbox__messages');
+        chatMessage.innerHTML = html;
+
+        chatMessage.scrollTop = chatMessage.scrollHeight;
+    }
+}
 
 const chatbox = new Chatbox();
-chatbox.display();
